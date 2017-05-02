f255e388312c31db7afba6bc298445502fc8bd5d
ulisesrmzroche <ulisesrmzroche@gmail.com>
2017-05-02 18:30:16 -0500

    export reminders and remove backlog tag

--- src/modules/osx/actions/index.js
+++ src/modules/osx/actions/index.js
@@ -1,13 +1,39 @@
+import { write } from './../utils';
+
 const Reminders = Application('Reminders');
 
 export const getBacklogReminders = () => {
-  console.log('geting reminders...')
-
-  const reminders = Reminders.reminders.whose({
+  console.log('geting reminders...');
+  return Reminders.reminders.whose({
     _and: [
       { name: { _contains: '#backlog' } },
       { completed: false },
     ],
   });
-  return reminders;
+};
+
+export const deleteReminders = (reminders) => {
+  console.log('deleting reminders...');
+  Reminders.delete(reminders);
+};
+
+export const writeRemindersToJSON = (reminders) => {
+  console.log('writing reminders to json...');
+  write(JSON.stringify({
+    reminders,
+  }), './dist/reminders.json');
+};
+
+export const processReminders = (reminders) => {
+  console.log('processing reminders...');
+  const _reminders = [];
+  reminders().forEach((reminder) => {
+    const _reminder = {
+      name: reminder.name().replace(/#backlog/g, ''),
+      body: reminder.body(),
+    };
+    console.log(`Creating reminder...: ${_reminder.name}`);
+    _reminders.push(_reminder);
+  });
+  return _reminders;
 };
--- src/modules/osx/reminders/index.js
+++ src/modules/osx/reminders/index.js
@@ -1,23 +1,12 @@
-import { write } from './../utils';
-import { getBacklogReminders, deleteReminders } from './../actions';
+import {
+  getBacklogReminders,
+  deleteReminders,
+  writeRemindersToJSON,
+  processReminders,
+} from './../actions';
 
 export default () => {
   const reminders = getBacklogReminders();
-  const _reminders = [];
-
-  reminders().forEach((reminder) => {
-    const _reminder = {
-      name: reminder.name(),
-      body: reminder.body(),
-    };
-    console.log(`Creating reminder...: ${_reminder.name}`);
-    _reminders.push(_reminder);
-  });
-
-  console.log('writing reminders to json...');
-  write(JSON.stringify({
-    reminders: _reminders,
-  }), './dist/reminders.json');
-
+  writeRemindersToJSON(processReminders(reminders));
   deleteReminders(reminders);
 };
